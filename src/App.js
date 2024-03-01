bb0f8615226910e2b4dad8c1822368d1e942d96c
Shivam Pandey <shivamkumarpandey3971@gmail.com>
2024-03-01 19:37:47 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import About from './components/About';
+import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
@@ -26,6 +26,12 @@ function App() {
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been enabled", "success");
       document.title = 'TextUtils - Dark Mode';
+      // setInterval(() => {
+      //   document.title = 'TextUtils is Amazing Mode';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = 'Install TextUtils Now';
+      // }, 1500);
     }
     else{
       setMode('light');
@@ -37,20 +43,16 @@ function App() {
 
   return (
     <>
-      {/* <Router> */}
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert}/>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert}/>
         <div className="container my-3">
-          {/* <Routes>
-            <Route path="/about" element={<About />} /> */}
-            {/* <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} /> */}
-          {/* </Routes> */}
-          <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
-          {/* <About/>
-           */}
-          
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
+          </Routes>
         </div>
-      {/* </Router> */}
+      </Router>
     </>
   );
 }
